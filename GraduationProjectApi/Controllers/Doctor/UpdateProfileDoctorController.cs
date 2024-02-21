f39e5e1f9f5999f388abe386a08725744c055c62
Abdelrahman Saber <109411513+AbdelrahmanSaber74@users.noreply.github.com>
2024-02-21 17:10:16 +0200

    Add FindUserController

--- GraduationProjectApi/Controllers/Doctor/UpdateProfileDoctorController.cs
+++ GraduationProjectApi/Controllers/Doctor/UpdateProfileDoctorController.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
-namespace GraduationProjectApi.Controllers.Students
+namespace GraduationProjectApi.Controllers.Doctor
 {
     [Route("api/[controller]")]
     [ApiController]
